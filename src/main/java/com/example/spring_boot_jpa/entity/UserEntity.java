216039de7f9a2fe53811fde2d78e2709d0778434
imooxo <kimsi1127@gmail.com>
2024-05-29 14:18:25 +0900

    Spring-JAP:jpa연결_24.05.29

--- src/main/java/com/example/spring_boot_jpa/entity/UserEntity.java
+++ src/main/java/com/example/spring_boot_jpa/entity/UserEntity.java
@@ -7,11 +7,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 // 실제 테이블 정의
-@Entity // 해당 클래스가 Entity임을 명시하는 어노테이션
-// 기본 생성자를 필요로 함
-@Table(name = "user") // user 라는 테이블과 해당 클래스를 매핑
+@Entity // 해당 클래스가 Entity임을 명시. 기본 생성자를 필요로 함.
+@Table(name = "user") // user라는 테이블과 해당 클래스를 매핑
 @Getter
-@Builder // 전체 필드를 받는 생성자를 필요로 함
+@Builder // 전체 필드를 받는 생성자를 필요로 함.
 @NoArgsConstructor
 @AllArgsConstructor
 // Entity 와 Builder를 동시에 사용하게 될 경우, 해당 어노테이션이 필요로 하는 생성자들을 명시
